e5c7decb5513d2a0b97744e514ad32c28319394d
Thanh Tran <mr.thanh.tct@gmail.com>
2015-11-24 22:09:54 +0100

    Add Problem Requirement Add Size of Character

--- prob2.1/main.c
+++ prob2.1/main.c
@@ -2,6 +2,9 @@
 //  main.c
 //  prob2.1
 //
+//  Use sizeof() operator to print the sizes of the basic
+//  and some composed data types to screen
+//
 //  Created by Thanh C. Tran on 24/11/15.
 //  Copyright © 2015 Thanh C. Tran. All rights reserved.
 //
@@ -10,6 +13,10 @@
 
 int main(int argc, const char * argv[]) {
     // insert code here...
+    char c = 'A';
+    printf("Size of char: %lu\n", sizeof(c));
+    
+    
     printf("Hello, World!\n");
     return 0;
 }
